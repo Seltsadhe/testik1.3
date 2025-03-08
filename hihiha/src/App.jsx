6413910f9ce679bbd583badc35cab2c3e621197f
Seltsadhe <seltsadge@gmail.com>
2025-03-08 12:08:18 +0200

    d

--- hihiha/src/App.jsx
+++ hihiha/src/App.jsx
@@ -2,6 +2,7 @@ import GoalList from "./GoalList";
 
 function App() {
   return (
+    
     <div className="App">
       <h1 className="text-center text-xl font-bold mt-4">My Goals</h1>
       <GoalList />
